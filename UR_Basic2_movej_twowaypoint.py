2909955f9ba583fdec6bf4b7e054656d08d99e31
Hsieh-Yu (Shay) Li <hsiehyulee@gmail.com>
2019-07-31 12:58:42 +0800

    movej_move with two way points

---
+++ UR_Basic2_movej_twowaypoint.py
@@ -0,0 +1,30 @@
+# UR 10 - Basic Operation 2- movej _ one point
+# Python 3.5
+# UR robot is the server, host computer is the client
+
+import socket
+import time
+
+# set up your communication with UR
+### change me - same as your UR controller IP
+HOST_ROBOT = "192.168.1.120"
+PORT_ROBOT = 30002
+###
+
+print("Starting Program.")
+
+# Communication - create_connection
+robot = socket.create_connection((HOST_ROBOT, PORT_ROBOT))
+time.sleep(2) # wait for 2 second
+print("Connected.")
+
+# way-point
+robot.send(b'movej([0.0,-1.57,0.0,-1.57,0.0,0.0],a=2.0,v= 0.5,r=0.01)\n') #joint commands, unit: radian
+time.sleep(5) # wait for 5 second
+
+robot.send(b'movej([ 0.22,-1.35079633  ,0.22,-1.35079633 ,-0.22,0],a=1.0,v= 0.05,r=0.01)\n')
+time.sleep(5) # wait for 5 second
+
+# close the communication
+robot.close()
+print("Close the program.")
